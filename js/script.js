d312a2a46631b4da5edafb088d779e62b794d66a
Davide Termite <davide.termite@gmail.com>
2020-05-26 17:23:41 +0200

    Script v0.2

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ var surname = ["Bianchi", "Rossi", "Verdi", "Nesta", "Baresi", "Maldini"];
 var userSurname = "";
 
 // Inserire cognome User nella Lista
-while (userSurname == "") {
-  userSurname = prompt ("Inserisci il tuo cognome")
-  surname.push(userSurname);
+do {
+  userSurname = prompt ("Inserisci il tuo cognome");
+} while ((!isNaN(userSurname)) || (userSurname === "")) {
+  var userUppercase = userSurname.toUpperCase()
+  surname.push(userUppercase);
   surname.sort();
 }
 
 // Output per User
 alert("Il tuo cognome fa parte di questa lista: " + surname);
-alert("Il tuo cognome è il numero " + (surname.indexOf(userSurname) + 1) + " su " + surname.length)
+alert("Il tuo cognome è il numero " + (surname.indexOf(userUppercase) + 1) + " su " + surname.length)
